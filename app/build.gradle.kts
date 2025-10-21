09a710a9cb88b5cd999ce2eae761137127b6350d
Luft21 <jfebryan15@gmail.com>
2025-10-21 15:12:50 +0700

    Versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.owoo"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
